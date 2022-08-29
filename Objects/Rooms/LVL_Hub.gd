0170d01ad7688f32a0505d92b7492955b8e7e554
E.Hill <hillendymion@gmail.com>
2022-08-29 15:16:43 -0700

    objects update.
    more npcs, and junk. also audio

--- Objects/Rooms/LVL_Hub.gd
+++ Objects/Rooms/LVL_Hub.gd
@@ -1,8 +1,20 @@
 extends Node2D
 #Enter1, Enter2
 onready var stats = Player_Stats
+onready var bulletManager = BulletManager
+onready var player = $Plyr #?
+export(String, FILE, "*.ogg") var song #this needs to be an object.
 
 func _ready() -> void:
+	#I should check if its a valid song.
+	if song:
+		print(str(song))
+		Player_Stats.newsong(song) #or song != null
+	#It might be better to emit a signal, but damn If I can wrap my head around that.
+	if player:
+		player.connect("Player_Bullet_Fired", bulletManager, "handle_bullet_Spawned")
+	
+	
 	print(stats.Door_Name)
 	if stats.Door_Name:
 		var enter_node = find_node(stats.Door_Name)
